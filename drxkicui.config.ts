bb2754aabf6b1659f166c6312b3412793e91a15d
Aidan Drekic <aidan.drekic@agf.com>
2024-10-03 16:57:32 -0400

    updated tailwind config and drxkic ui config to now accept in an array of specific colors.

--- drxkicui.config.ts
+++ drxkicui.config.ts
@@ -1,6 +1,19 @@
+export const Colors: Record<string, string> = {
+	red: "#880808",
+	brown: "#8d6e63",
+	yellow: "#f4d03f",
+	green: "#1d8348",
+	purple: "#bb8fce",
+	orange: "#f5b041",
+	blue: "#2D55E2",
+	lightGreen: "#82e0aa",
+	black: "#000000",
+	white: "#FFFFFF",
+}
+
 export default {
-	accent: "#97A0BF",
-	base: "#364937",
+	colors: Colors,
+	base: "#000000",
 	lightBackground: "#FFFFFF",
 	darkBackground: "#111111",
 }
